0cf7fc1cc3d82ae2e34c3f91b4f3c31a07156359
oliveirabz <brunabzoliv@gmail.com>
2025-03-08 04:40:23 -0300

    fix: all sass warnings fixed

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  sassOptions: {
-    quietDeps: true,
-  },
 };
 
 export default nextConfig;
